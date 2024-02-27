bdc4a39e8faa725e2dae4935f863eba66918431a
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-02-26 16:51:03 -0800

    creating flight class file

---
+++ flight.java
@@ -0,0 +1,16 @@
+public class flight {
+    private String name;
+    private String date;
+    private String cost;
+    private String departure;
+    private String arrival;
+    private String duration;
+    public flight (String name, String date, String cost, String departure, String arrival, String duration) {
+        this.name = name;
+        this.date = date;
+        this.cost = cost;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.duration = duration;
+    }
+}
