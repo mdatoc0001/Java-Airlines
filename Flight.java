8368580eaace9dea4cc0e61eb7252f7fc7a49a8f
mdatoc0001 <112523992+mdatoc0001@users.noreply.github.com>
2024-02-28 16:38:07 -0800

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,16 +1,61 @@
 public class Flight {
-    protected String name;
-    protected String date;
-    protected String cost;
-    protected String departure;
-    protected String arrival;
-    protected String duration;
-    public Flight (String name, String date, String cost, String departure, String arrival, String duration) {
+    private String name;
+    private String date;
+    private String cost;
+    private String departure;
+    private String arrival;
+    private String duration;
+    private Flight (String name, String date, String cost, String departure, String arrival, String duration) {
         this.name = name;
         this.date = date;
         this.cost = cost;
         this.departure = departure;
         this.arrival = arrival;
+        this.duration = duration;
+    }
+    
+    //Getters
+    public String getName() {
+         return this.name;
+    }
+    public String getDate() {
+         return this.date;
+    }
+    public String getCost() {
+         return this.cost;
+    }
+    public String getDeparture() {
+         return this.departure;
+    }
+    public String getArrival() {
+         return this.arrival;
+    }
+    public String getDuration() {
+         return this.duration;
+    }
+    
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public void setDuration(String duration) {
         this.duration = duration;
     }
 }
