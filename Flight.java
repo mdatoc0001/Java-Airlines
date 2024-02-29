ce5d222b96088249961f77f668fe79d1e3c5301d
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-02-28 17:29:54 -0800

    added more parameters

--- Flight.java
+++ Flight.java
@@ -1,17 +1,25 @@
 public class Flight {
     private String name;
     private String date;
-    private String cost;
+    private double cost;
     private String departure;
     private String arrival;
     private String duration;
-    public Flight (String name, String date, String cost, String departure, String arrival, String duration) {
+    private int totalSeats;
+    private int takenSeats;
+    private int availableSeats;
+    private String seatNumber;
+    public Flight (String name, String date, double cost, String departure, String arrival, String duration, int totalSeats, int takenSeats, int availableSeats, String seatNumber) {
         this.name = name;
         this.date = date;
         this.cost = cost;
         this.departure = departure;
         this.arrival = arrival;
         this.duration = duration;
+        this.totalSeats = totalSeats;
+        this.takenSeats = takenSeats;
+        this.availableSeats = availableSeats;
+        this.seatNumber = seatNumber;
     }
     
     //Getters
@@ -21,7 +29,7 @@ public class Flight {
     public String getDate() {
          return this.date;
     }
-    public String getCost() {
+    public double getCost() {
          return this.cost;
     }
     public String getDeparture() {
@@ -33,29 +41,48 @@ public class Flight {
     public String getDuration() {
          return this.duration;
     }
+    public int getTotalSeats() {
+        return this.totalSeats;
+    }
+    public int getTakenSeats() {
+        return this.takenSeats;
+    }
+    public int getAvailableSeats() {
+        return this.availableSeats;
+    }
+    public String getSeatNumber() {
+        return this.seatNumber;
+    }
     
     //Setters
     public void setName(String name) {
         this.name = name;
     }
-
     public void setDate(String date) {
         this.date = date;
     }
-
-    public void setCost(String cost) {
+    public void setCost(double cost) {
         this.cost = cost;
     }
-
     public void setDeparture(String departure) {
         this.departure = departure;
     }
-
     public void setArrival(String arrival) {
         this.arrival = arrival;
     }
-
     public void setDuration(String duration) {
         this.duration = duration;
+    }
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+    public void setTakenSeats(int takenSeats) {
+        this.takenSeats = takenSeats;
+    }
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+    public void setSeatName(String seatName) {
+        this.seatName = seatName;
     }
 }
