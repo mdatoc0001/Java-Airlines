7fc3fa831e89315f6fcb8ae52db987ccbf205dd9
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-02-28 16:18:43 -0800

    updated variables

--- flight.java
+++ flight.java
@@ -1,16 +1,27 @@
 public class Flight {
+    // declaring variables
     protected String name;
     protected String date;
-    protected String cost;
+    protected double cost;
     protected String departure;
     protected String arrival;
     protected String duration;
-    public Flight (String name, String date, String cost, String departure, String arrival, String duration) {
+    protected int totalSeats;
+    protected int takenSeats;
+    protected int availableSeats;
+    protected String seatNumber;
+
+    public Flight (String name, String date, double cost, String departure, String arrival, String duration, int totalSeats, int takenSeats, int availableSeats, String seatNumber) {
         this.name = name;
         this.date = date;
         this.cost = cost;
         this.departure = departure;
         this.arrival = arrival;
         this.duration = duration;
+        this.totalSeats = totalSeats;
+        this.takenSeats = takenSeats;
+        this.availableSeats = availableSeats;
+        this.seatNumber = seatNumber;
     }
+
 }
