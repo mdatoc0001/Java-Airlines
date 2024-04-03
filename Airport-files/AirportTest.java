812da33e0f6fad4ab87e7a1f037ff7fa487adedb
mdatoc0001 <112523992+mdatoc0001@users.noreply.github.com>
2024-04-03 16:29:43 -0700

    Update AirportTest.java

--- Airport-files/AirportTest.java
+++ Airport-files/AirportTest.java
@@ -23,31 +23,33 @@ public class AirportTest {
    /** Airport class tests **/
    @Test
    public void testGetAirportName() {
+      test.makeFlight("Flight1", "LAX", "BNA", "2024-03-10", "2024-03-10", "Departure1", "Arrival1",
+                "Terminal1", 1, 150, 50, 100, "$200", "2 hours", false);
       assertEquals("LAX", test.getName());
    }
    @Test
    public void testMakeFlight() {
-      test.makeFlight("Flight1", "2024-03-10", "$200", "Destination1", "Departure1", "Arrival1",
-                      "2 hours", 150, 50, 100, "A1", "Terminal1", "Gate1");
+      test.makeFlight("Flight1", "LAX", "BNA", "2024-03-10", "2024-03-10", "Departure1", "Arrival1",
+                "Terminal1", 1, 150, 50, 100, "$200", "2 hours", false);
       assertTrue(test.getFlights() != null); // Assert there are flights for airport
    }
    @Test
    public void testGetNumFlights() {
-      test.makeFlight("Flight1", "2024-03-10", "$200", "Destination1", "Departure1", "Arrival1",
-                      "2 hours", 150, 50, 100, "A1", "Terminal1", "Gate1");
+      test.makeFlight("Flight1", "LAX", "BNA", "2024-03-10", "2024-03-10", "Departure1", "Arrival1",
+                "Terminal1", 1, 150, 50, 100, "$200", "2 hours", false);
       assertEquals(1, test.getNumFlights());
    }
    @Test
    public void testSetName() {
-      test.makeFlight("Flight1", "2024-03-10", "$200", "Destination1", "Departure1", "Arrival1",
-                      "2 hours", 150, 50, 100, "A1", "Terminal1", "Gate1");
+      test.makeFlight("Flight1", "LAX", "BNA", "2024-03-10", "2024-03-10", "Departure1", "Arrival1",
+                "Terminal1", 1, 150, 50, 100, "$200", "2 hours", false);
       test.setName("John Wayne Airport");
       assertEquals("John Wayne Airport", test.getName());
    }
    @Test
    public void testGetFlights() {
-      test.makeFlight("Flight1", "2024-03-10", "$200", "Destination1", "Departure1", "Arrival1",
-                      "2 hours", 150, 50, 100, "A1", "Terminal1", "Gate1");
+      test.makeFlight("Flight1", "LAX", "BNA", "2024-03-10", "2024-03-10", "Departure1", "Arrival1",
+                "Terminal1", 1, 150, 50, 100, "$200", "2 hours", false);
       Flight[] flights = test.getFlights();
       assertEquals("Flight1", flights[0].getName()); 
    }
