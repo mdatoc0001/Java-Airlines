00d6ab194d811d6f685a49f88f44909d6bd1d4f9
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-04-22 19:18:34 -0700

    adding file reader

--- Manager.java
+++ Manager.java
@@ -7,6 +7,11 @@ import java.util.Arrays;
 public class Manager {
     // classifying attributes
 
+	UserInfoParser parser = new UserInfoParser();
+	String filePath = "src//application//UserInfo.txt";
+	List<UserInfo> userInfoList = parser.readUserDataFromTXT(filePath);
+	
+	
     private String name = "Manager";
     private Flight flight;
     private Airport airport;
@@ -26,4 +31,9 @@ public class Manager {
         return numFlightsOpen;
     }
     
+    public static void main(String[] Args) {
+    	System.out.println(userInfoList);
+    }
+    
 }
+
