a073430e7773ba97258f8c8fbf8cd93ec2db5291
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-04-29 15:04:39 -0700

    making confirmation page

---
+++ JavaAirlines2/src/application/ConfirmationSceneController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ConfirmationSceneController {
+
+    @FXML
+    private Label confirmationNo;
+
+    @FXML
+    void Logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Yes(ActionEvent event) {
+
+    }
+
+}
