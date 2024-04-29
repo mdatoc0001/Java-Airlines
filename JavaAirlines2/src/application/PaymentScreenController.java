bc2c8c8011ed5428f313879ef87242dd1a8ce67d
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-04-29 14:42:46 -0700

    implements fx fields for payment gui

--- JavaAirlines2/src/application/PaymentScreenController.java
+++ JavaAirlines2/src/application/PaymentScreenController.java
@@ -1,24 +1,63 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class PaymentScreenController {
+public class PaymentScreenController implements Initializable {
 
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
-	@FXML
+    @FXML
+    private TextField cardName;
+
+    @FXML
+    private TextField cardNo;
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @FXML
+    private Label cost;
+
+    @FXML
+    private TextField cvv;
+
+    @FXML
+    private TextField validityDate;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    	
+    	comboBox.setItems(FXCollections.observableArrayList("Debit","Credit"));
+    	
+    }
+    
+    @FXML
+    public void getComboBoxInfo(ActionEvent event) {
+    	System.out.println(comboBox.getValue());
+    }
+    
+    @FXML
     void Back(ActionEvent event) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("AirportSelection.fxml"));
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("AirportSelection.fxml"));
 		root = loader.load();
 		
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -26,5 +65,13 @@ public class PaymentScreenController {
 		stage.setScene(scene);
 		stage.show();
     }
-	
+
+    @FXML
+    void Pay(ActionEvent event) {
+
+    }
+
 }
+	
+	
+	
