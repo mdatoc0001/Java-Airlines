6c72d76b283db209c1d6064f0545e320109bd61a
Madeline Luna <madeline.luna.850@my.csun.edu>
2024-04-29 12:28:31 -0700

    updated manager Terminate functions

--- JavaAirlines2/src/application/ManagerTerminateController.java
+++ JavaAirlines2/src/application/ManagerTerminateController.java
@@ -1,32 +1,69 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.ArrayList;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class ManagerTerminateController {
+public class ManagerTerminateController implements Initializable{
 
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
 	Manager manager = new Manager();
+	List<String> listOfNames;
+	
+	@FXML
+    private Label textBox1;
+
+    @FXML
+    private Label textBox2;
 	
     @FXML
-    private TextField flightName;
+    private ComboBox<String> comboBox;
     @FXML
     private Label confirmationText;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    	FlightParser parser2 = new FlightParser();
+    	List<Flight> flights = null;
+		try {
+			flights = parser2.readFlightsFromTXT("src//application//Flights.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(flights);
+		
+    	listOfNames = manager.getAllFlights(flights);
+    	System.out.println(listOfNames);
+    	ObservableList<String> list = FXCollections.observableArrayList(listOfNames);
+    	System.out.println(list);
+    	comboBox.setItems(list);
+    	
+    }
+    
+    @FXML
+    public void getComboBoxInfo(ActionEvent event) {
+    	System.out.println(comboBox.getValue());
+    }
     
     @FXML
     public void Back(ActionEvent event) throws IOException {
@@ -40,13 +77,60 @@ public class ManagerTerminateController {
     }
 
     @FXML
+    public void chooseFlight(ActionEvent event) throws IOException {
+    	String name = comboBox.getValue();
+    	FlightParser parser2 = new FlightParser();
+    	List<Flight> flights = parser2.readFlightsFromTXT("src//application//Flights.txt");
+    	Flight flight = manager.getFlight(flights, name);
+    	
+    	if (name == null) {
+    		textBox1.setText("No Flights Reserved");
+    	} else {
+    		textBox1.setText(
+        			"Flight Number: " + flight.getName() + "\n" +
+        			"Departing City: " + flight.getDepartCity() + "\n" +
+        			"Arrival City: " + flight.getArrivalCity() + "\n" +
+        			"Departing Date: " + flight.getDepartDate() + "\n" +
+        			"Arrival Date: " + flight.getArrivalDate() + "\n" +
+        			"Departing Time: " + flight.getDepartTime() + "\n" +
+        			"Arrival Time: " + flight.getArrivalTime());
+        	textBox2.setText(
+        			"Terminal: " + flight.getTerminal() + "\n" + 
+        			"Gate: " + flight.getGate() + "\n" +
+    				"Total Seats: " + flight.getTotalSeats() + "\n" +
+    				"Taken Seats: " + flight.getTakenSeats() + "\n" +
+    				"Available Seats: " + flight.getAvailableSeats() + "\n" +
+    				"Cost: " + flight.getCost() + "\n" +
+    				"Duration: " + flight.getDuration() + "\n" +
+    				"Booking Status: " + flight.getBookStatus());
+    	}
+    }
+    
+    @FXML
     public void Submit(ActionEvent event) throws IOException {
-    	String name = flightName.getText();
+    	String name = comboBox.getValue();
     	FlightParser parser2 = new FlightParser();
     	List<Flight> flights = parser2.readFlightsFromTXT("src//application//Flights.txt");
     	Flight flight = manager.getFlight(flights, name);
     	manager.terminateFlight(flight);
     	System.out.print(flight);
+    	textBox1.setText(
+    			"Flight Number: " + flight.getName() + "\n" +
+    			"Departing City: " + flight.getDepartCity() + "\n" +
+    			"Arrival City: " + flight.getArrivalCity() + "\n" +
+    			"Departing Date: " + flight.getDepartDate() + "\n" +
+    			"Arrival Date: " + flight.getArrivalDate() + "\n" +
+    			"Departing Time: " + flight.getDepartTime() + "\n" +
+    			"Arrival Time: " + flight.getArrivalTime());
+    	textBox2.setText(
+    			"Terminal: " + flight.getTerminal() + "\n" + 
+    			"Gate: " + flight.getGate() + "\n" +
+				"Total Seats: " + flight.getTotalSeats() + "\n" +
+				"Taken Seats: " + flight.getTakenSeats() + "\n" +
+				"Available Seats: " + flight.getAvailableSeats() + "\n" +
+				"Cost: " + flight.getCost() + "\n" +
+				"Duration: " + flight.getDuration() + "\n" +
+				"Booking Status: " + flight.getBookStatus());
     	confirmationText.setText("Successfully Terminated!");
     }
 
